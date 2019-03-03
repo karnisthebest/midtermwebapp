66f3a856b17f7698612d50698c5273adc0a566f3
karnisthebest <karnisthebest@gmail.com>
2019-03-03 22:26:04 +0700

    new

--- Models/MidtermDbContext.cs
+++ Models/MidtermDbContext.cs
@@ -9,5 +9,4 @@ public class MidtermDbContext: DbContext
    public DbSet<Product> Products { get; set; } //this is use to create an reference with table = "Products"
    public DbSet<Supplier> Suppliers { get; set; }
 
-   public DbSet<SaleOrder> SaleOrders { get; set; }
 }
